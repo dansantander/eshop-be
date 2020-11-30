5c0f91b4fafec2a634c0c090bede1a4c66cbc7f8
Daniel Santander <danyvo1d@gmail.com>
2020-11-30 10:40:28 -0600

    feat: add session creation

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,40 @@
+class SessionsController < ApplicationController
+  #include CurrentUserConcern
+
+  def create
+    user = User
+      .find_by(email: params["user"]["email"])
+      .try(:authenticate, params["user"]["password"])
+    if user
+      session[:user_id] = user.id
+      render json: {
+        status: :created, # this is the same as 201 in HTTP Status Code
+        logged_in: true,
+        user: user
+      }
+    else 
+      render json: {
+        status: 401 # this could also be written as :unauthorized
+      }
+    end
+  end
+
+=begin   def logged_in
+    if @current_user
+      render json: {
+        logged_in: true,
+        user: @current_user
+      }
+    else 
+      render json: {
+        logged_in: false
+      }
+    end
+  end
+
+  def logged_out
+    reset_session
+    render json: { status: 200, logged_out: true}
+  end
+=end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_163042) do
+ActiveRecord::Schema.define(version: 2020_11_30_161008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2020_11_27_163042) do
     t.text "description", default: "", null: false
     t.decimal "price", precision: 5, scale: 2, null: false
     t.string "image", default: "https://placeimg.com/480/640/tech"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
