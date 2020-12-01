78243bdb243c46efbed7bb0f812d4b7dac912387
dansantander <dansantandr@gmail.com>
2020-12-01 16:47:06 -0600

    feat: add favorite model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :favorites
+  has_many :products, through: :favorites
+
   validates :username, presence: true
   validates :email, uniqueness: true, presence: true
 end
