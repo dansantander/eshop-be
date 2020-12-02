4b3c377ff5c0f198af0419ed8a131f5dad9240c7
dansantander <dansantandr@gmail.com>
2020-12-02 15:00:40 -0600

    fix: favorite products list output

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,8 @@ class FavoritesController < ApplicationController
   include CurrentUserConcern
 
   def index
-    @favorites = Favorite.where(user_id: @current_user.id)
+    # @favorites = Favorite.where(user_id: @current_user.id)
+    @favorites = @current_user.products
     render json: {favProducts: @favorites }, status: :ok
   end
 end
