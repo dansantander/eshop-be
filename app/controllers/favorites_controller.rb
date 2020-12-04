5b111a434ab12022d72a67266d439a2261f84bf8
dansantander <dansantandr@gmail.com>
2020-12-04 13:52:47 -0600

    feat: created favorite from front end successfully

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,8 +2,27 @@ class FavoritesController < ApplicationController
   include CurrentUserConcern
 
   def index
-    # @favorites = Favorite.where(user_id: @current_user.id)
     @favorites = @current_user.products
     render json: {favProducts: @favorites }, status: :ok
+  end
+
+  def create
+    favorite = Favorite.create!(
+      user_id: params["user_id"],
+      product_id: params["product_id"],
+    )
+    if favorite
+      render json: {
+        status: :created,
+        favorite: favorite,
+      }
+    else 
+      render json: {
+        status: 500
+      }
+    end
+  end
+
+  def destroy
   end
 end
