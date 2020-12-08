84a7b5b714d6f10a4509b356f590e81e8b4c4d17
dansantander <dansantandr@gmail.com>
2020-12-08 11:14:31 -0600

    fix: favorites create and remove response

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,16 +2,17 @@ class FavoritesController < ApplicationController
   include CurrentUserConcern
 
   def index
-    @favorites = @current_user.products
-    render json: {favProducts: @favorites }, status: :ok
+    favorites = @current_user.products
+    render json: {favProducts: favorites }, status: :ok
   end
 
   def create
     favorite = Favorite.create!(user_id: @current_user.id, product_id: params["product_id"])
+    favorites = @current_user.products
     if favorite
       render json: {
         status: :created,
-        favorite: favorite,
+        favProducts: favorites,
       }
     else 
       render json: { status: 500 }
@@ -20,8 +21,12 @@ class FavoritesController < ApplicationController
 
   def destroy
     favorite = Favorite.find_by(user_id: @current_user.id, product_id: params["id"])
+    favorites = @current_user.products
     if favorite.destroy
-      render json: { status: 'removed' }
+      render json: {
+        status: :removed,
+        favProducts: favorites
+      }
     else
       render json: { status: 500 }
     end
