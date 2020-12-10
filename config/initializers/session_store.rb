b4b342e80f13d604c73d9dd0bb1a0cf5693ee076
dansantander <dansantandr@gmail.com>
2020-12-10 16:22:22 -0600

    fix: same site on rails.application.config line

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,7 @@
 # We define what the cookies are going to be structured like
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_eshop_be_app",
+  Rails.application.config.session_store :cookie_store, same_site: :none, secure: true, key: "_eshop_be_app",
   domain: "https://eshop-be-1418.herokuapp.com"
-  same_site: :none
-  secure: true
 else
   Rails.application.config.session_store :cookie_store, key: "_eshop_be_app"
 end
