bdad7b99e86971ff21e2434ee195b3babc396a27
dansantander <dansantandr@gmail.com>
2020-12-12 16:53:38 -0600

    chore: trying same_site none after domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # We define what the cookies are going to be structured like
 if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, same_site: :none, secure: true, key: "_eshop_be_app", tld_length: 2,
-  domain: "https://eshop-be-1418.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, key: "_eshop_be_app", tld_length: 2,
+  domain: "https://eshop-be-1418.herokuapp.com", same_site: :none, secure: true
 else
   Rails.application.config.session_store :cookie_store, key: "_eshop_be_app"
 end
