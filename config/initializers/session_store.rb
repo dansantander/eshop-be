4f35e8233280ce93170d06c40965e3516190f649
dansantander <dansantandr@gmail.com>
2020-12-12 16:48:42 -0600

    chore: testing env production method

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 # We define what the cookies are going to be structured like
-if Rails.env == "production"
+if Rails.env.production
   Rails.application.config.session_store :cookie_store, same_site: :none, secure: true, key: "_eshop_be_app", tld_length: 2,
   domain: "https://eshop-be-1418.herokuapp.com"
 else
