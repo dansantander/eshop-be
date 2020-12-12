aeb4e0d7a06a3da34b5e51c5e10aab98ebe7aa66
dansantander <dansantandr@gmail.com>
2020-12-12 16:37:57 -0600

    chore: trying with tld

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # We define what the cookies are going to be structured like
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, same_site: :none, secure: true, key: "_eshop_be_app",
+  Rails.application.config.session_store :cookie_store, same_site: :none, secure: true, key: "_eshop_be_app", tld_length: 2,
   domain: "https://eshop-be-1418.herokuapp.com"
 else
   Rails.application.config.session_store :cookie_store, key: "_eshop_be_app"
