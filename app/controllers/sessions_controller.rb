287bfc7ffca0549e636ac43e13162542ee2dd7ba
dansantander <dansantandr@gmail.com>
2020-12-12 16:05:08 -0600

    chore: back to session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,12 @@
 class SessionsController < ApplicationController
-  # include CurrentUserConcern
+  include CurrentUserConcern
 
   def create
     user = User
       .find_by(email: params['user']['email'])
       .try(:authenticate, params['user']['password'])
     if user
-      # session[:user_id] = user.id
-      cookies[:user_id] = user.id
+      session[:user_id] = user.id
       render json: {
         status: :created, # this is the same as 201 in HTTP Status Code
         logged_in: true,
@@ -34,8 +33,7 @@ class SessionsController < ApplicationController
   end
 
   def logged_out
-    # reset_session
-    cookie.delete(:user_id)
+    reset_session
     render json: { status: 200, logged_out: true }
   end
 end
