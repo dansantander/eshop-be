e30ff09c94109f6138d61c3c55f0f7a40ce276eb
dansantander <dansantandr@gmail.com>
2020-12-12 17:13:03 -0600

    fix: current user concern back to sessions

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -6,7 +6,6 @@ module CurrentUserConcern
   end
 
   def set_current_user
-    # @current_user = User.find(session[:user_id]) if session[:user_id]
-    @current_user = User.find(cookies[:user_id]) if cookies[:user_id]
+    @current_user = User.find(session[:user_id]) if session[:user_id]
   end
 end
