72bb99b8364670b2c1522dd4a020c9b6d93ea8df
dansantander <dansantandr@gmail.com>
2020-12-13 10:04:18 -0600

    chore: going back to original config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,5 +3,5 @@ if Rails.env.production?
   Rails.application.config.session_store :cookie_store, key: "_eshop_be_app", tld_length: 2,
   domain: "https://eshop-be-1418.herokuapp.com", same_site: :none, secure: true
 else
-  Rails.application.config.session_store :cookie_store, key: "_eshop_be_app", same_site: :none, secure: true
+  Rails.application.config.session_store :cookie_store, key: "_eshop_be_app"
 end
